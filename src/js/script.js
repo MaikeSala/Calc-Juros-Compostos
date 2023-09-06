21c24e5d821f84f43402fc5d932b953c1025e08f
Maike Sala <maikesala49@gmail.com>
2023-09-06 19:47:41 -0300

    Lógica Finaliza

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,58 @@
 const botao = document.querySelector(".simular");
 
-botao.addEventListener('click', (e) => {
+function elementosNaTela (aporteInical, aporteMensal, juros, anos) {
 
-    e.preventDefault();
+    const minhaDiv = document.getElementById('meu_calculo');
+    const spans = minhaDiv.querySelectorAll('span');
+
+    if(aporteInical.length === 0 || juros.length === 0 || anos.length === 0){
+        return;
+    }else{
+        spans.forEach((span) => {
+            minhaDiv.removeChild(span);
+        });
+    }
+
+    const aporteInicialSpan = document.createElement('span');
+    const aporteMensalSpan = document.createElement('span');
+    const jurosSpan = document.createElement('span');
+    const anosSpan = document.createElement('span');
+    
+    aporteInicialSpan.textContent = `Valor inicial: R$ ${aporteInical}`;
+    aporteMensalSpan.textContent = `Valor do aporte: R$${aporteMensal}`;
+    jurosSpan.textContent = `Taxa de juros: ${juros}% ao mês`;
+    anosSpan.textContent = `Período: ${anos} meses`;
+
+    minhaDiv.appendChild(aporteInicialSpan);
+    minhaDiv.appendChild(aporteMensalSpan);
+    minhaDiv.appendChild(jurosSpan);
+    minhaDiv.appendChild(anosSpan);
+}
+
+function resultadoNaTela(valorTotalFinal, valorTotalInvestido, jurosTotal) {
+    const minhaDiv = document.getElementById('resultado');
+    const spans = minhaDiv.querySelectorAll('span');
+
+    spans.forEach((span) => {
+        minhaDiv.removeChild(span);
+    });
+
+    const valorTotalFinalSpan = document.createElement('span');
+    const valorTotalInvestidoSpan = document.createElement('span');
+    const jurosTotalSpan = document.createElement('span');
+
+    valorTotalFinalSpan.textContent = `Valor total final: R$ ${valorTotalFinal.toFixed(2)}`;
+    valorTotalInvestidoSpan.textContent = `Valor total investido: R$${valorTotalInvestido.toFixed(2)}`;
+    jurosTotalSpan.textContent = `Total de juros ${jurosTotal.toFixed(2)}`;
+
+    minhaDiv.appendChild(valorTotalFinalSpan);
+    minhaDiv.appendChild(valorTotalInvestidoSpan);
+    minhaDiv.appendChild(jurosTotalSpan);
+
+}
+
+
+botao.addEventListener('click', (e) => {
 
     const juros = document.querySelector("#invest_porcentagen").value;
     const anos = document.querySelector('#tempo').value;
@@ -14,18 +64,20 @@ botao.addEventListener('click', (e) => {
     i = parseFloat(juros)/100;
     t = parseFloat(anos);  
 
-    console.log(i, t, C, PM);
     let M = 0;
-    //Multiplicar por 12 para saber quantos meses sao
-    t = t * 12;
-    let aux = 0;
 
     for(let j = 0; j < t; j++){
         if(isNaN(PM)) PM = 0;
 
         M = C * (1 + i)**1;
         C = M + PM;
-
-        console.log(C);
     }
+
+    let totalInvestido = parseFloat(aporteInical) + (PM * t);
+    let totalJuros = C - totalInvestido;
+
+    console.log(C.toFixed(2),totalInvestido,totalJuros); 
+
+    elementosNaTela(aporteInical, aporteMensal, juros, anos);
+    resultadoNaTela(C, totalInvestido, totalJuros);
 });
